2b7ca92a6232c9e6275b3cee435bb0c92a35b50b
rayydev <kontakt.rayy@outlook.com>
2021-09-16 21:20:52 +0200

    launcher code edited

--- scripts/launcher.cpp
+++ scripts/launcher.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+
+
 int main()
 {
     int select;
@@ -7,4 +9,21 @@ int main()
     std::cout << "1) Run ConAIO \n";
     std::cout << "2) Credits \n";
     std::cout << "3) Exit \n";
+
+    std::cin >> select;
+    
+    if (select == 1)
+    {
+        //script
+    }
+    if (select == 2)
+    {
+        system("clear");
+        std::cout << "Made by rxvyonline (rayydev) in c++ and batch \n";
+        return 0;
+    }
+    if (select == 3)
+    {
+        return 0;
+    }
 }
